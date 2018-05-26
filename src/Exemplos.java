a4dda83b21e2287ef488faffa672c598853581f6
Rafael <ferreira.rfl@gmail.com>
2018-05-26 18:34:46 -0300

    Add exemplo do square

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -1,6 +1,8 @@
 public class Exemplos {
     public static void main(String[] args) {
-        Exemplo01();
+        //Exemplo01();
+        drawTop();
+        drawBox(5,5);
     }
     //começo do exemplo 01
     public static void Exemplo01() {
@@ -19,4 +21,32 @@ public class Exemplos {
         }
     }
     //fim do exemplo 01
+    public static final int SUB_HEIGHT = 5;
+    public static void drawTop() {
+        for (int line = 1; line <= SUB_HEIGHT; line++) {
+            System.out.print("|");
+            writeChars(' ', line - 1);
+            System.out.print("\\");
+            writeChars('.', 2 * SUB_HEIGHT - 2 * line);
+            System.out.print("/");
+            writeChars(' ', line - 1);
+            System.out.println("|");
+        }
+    }
+
+    //exemplo 3 desenhando um quadrado
+    public static void drawBox(int height, int width) {
+        // draw top of box
+        writeChars('*', width);
+        System.out.println();
+        // draw middle lines
+        for (int i = 1; i <= height - 2; i++) {
+            System.out.print('*');
+            writeChars(' ', width - 2);
+            System.out.println("*");
+        }
+        // draw bottom of box
+        writeChars('*', width);
+        System.out.println();
+    }
 }
