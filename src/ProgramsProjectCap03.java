65265295d55a7b1187b0e732f8a559528dbf1f7c
Rafael <ferreira.rfl@gmail.com>
2018-05-28 21:00:49 -0300

    Programms projects Cap 03

--- src/ProgramsProjectCap03.java
+++ src/ProgramsProjectCap03.java
@@ -1,37 +1,23 @@
 public class ProgramsProjectCap03 {
     public static void main (String[] args){
-        programa1();
+        Program01(5, 3);
+        Program02();
     }
-    public static void programa1() {
-        for (int i = 6; i >= 1; i--) {
-            //Asteriscos a esquerda
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+
+    public static void Program01(int altura, int segmentos) {
+        for (int seg = 1; seg <= segmentos; seg++){
+            for (int line = seg; line <= (altura+seg-1) ; line++) {
+                for (int i = altura - line ; i >= 0; i--) {
+                    System.out.print(" ");
+                }
+
+                for (int i = 1; i <= ((line) * 2 - 1); i++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.print(" ");
-            //espaços a Esquerda
-            for (int espacos = 6; espacos >= i; espacos--) {
-                System.out.print(" ");
-            }
-            //barras a esquerda
-            for (int j = 1; j <= i*2; j++) {
-                System.out.print("/");
-            }
-            System.out.print(" ");
-            //barras a direita
-            for (int j = 12; j >= (i * 2) - 1; j--) {
-                System.out.print("\\");
-            }
-            System.out.print(" ");
-            //espaços a direita
-            for (int espacos = 6; espacos >= i; espacos--) {
-                System.out.print(" ");
-            }
-            //Asteriscos a direita
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
     }
+    
+
 }
